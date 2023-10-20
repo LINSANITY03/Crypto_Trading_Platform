731bf6001417795c7527cd6eafc70234ce1e3694
pujan tamang <pujantamang92@gmail.com>
2023-10-20 12:20:08 +0100

    created implementation of the constructor

---
+++ OrderBookEntry.cpp
@@ -0,0 +1,15 @@
+#include "OrderBookEntry.h"
+
+// class::function
+OrderBookEntry::OrderBookEntry(double price,
+                               double amount,
+                               std::string timestamp,
+                               std::string product,
+                               OrderBookType orderType)
+    : price(price),
+      amount(amount),
+      timestamp(timestamp),
+      product(product),
+      orderType(orderType)
+{
+}
